5170883d0f515769439fd550cb2007a317b78d52
= <=>
2016-06-20 17:09:57 -0400

    update populate method with text file and add bracket methods

--- board.rb
+++ board.rb
@@ -1,5 +1,5 @@
+require 'byebug'
 require_relative "piece"
-
 class Board
 
   attr_reader :grid
@@ -8,14 +8,38 @@ class Board
     populate
   end
 
+  def [](pos)
+    x,y = pos
+    grid[x][y]
+  end
+
+  def []=(pos, piece)
+    x,y = pos
+    grid[x][y] = piece
+  end
+
+  def move(start, end_pos)
+
+  end
+
   private
 
   def populate
-    grid.each_with_index do |row, i|
-      row.each_with_index do |column, j|
-        self.grid[i][j] = Piece.new([i, j])
+    positions = File.readlines("populate.txt")
+    #debugger
+    positions.each_with_index do |row, index|
+      unless row == "\n"
+        self.grid[index] = positions[index]
       end
     end
+
+
+
+    # grid.each_with_index do |row, i|
+    #   row.each_with_index do |column, j|
+    #     self.grid[i][j] = Piece.new([i, j])
+    #   end
+    # end
   end
 
 end
