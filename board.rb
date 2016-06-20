03cf733c2b78070206bfe412c674b2de6f222cc2
= <=>
2016-06-20 16:49:36 -0400

    Create dummy populate method and piece initializer

--- board.rb
+++ board.rb
@@ -5,8 +5,17 @@ class Board
   attr_reader :grid
   def initialize(grid = Array.new(8){ Array.new(8) } )
     @grid = grid
+    populate
   end
 
+  private
 
+  def populate
+    grid.each_with_index do |row, i|
+      row.each_with_index do |column, j|
+        self.grid[i][j] = Piece.new([i, j])
+      end
+    end
+  end
 
 end
--- piece.rb
+++ piece.rb
@@ -1,3 +1,20 @@
 class Piece
 
+  attr_reader :pos
+
+  def initialize(pos)
+    @pos = pos
+
+  end
+
+  #temp to_s method
+
+  def to_s
+    return "p"
+  end
+
+  # def pos
+  #   x,y = pos
+  # end
+
 end
